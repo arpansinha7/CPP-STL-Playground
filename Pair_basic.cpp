241080dedc777261652e5f95940b36f034d03e85
Arpan Sinha <itsarpansinha@hotmail.com>
2025-11-05 18:15:35 +0530

    Pair_basic

--- Pair_basic.cpp
+++ Pair_basic.cpp
@@ -5,22 +5,22 @@ int main()
 {
     pair <int, int> pr = {3, 5};
 
-    cout<<"First Integer :"<<pr.first<<endl;
-    cout<<"Second Integer :"<<pr.second<<endl;
+    cout<<"First Integer :"<<pr.first<<endl; // first returns the first element of pair
+    cout<<"Second Integer :"<<pr.second<<endl; // second returns the second element of pair
 
     pair <string, int> sr = {"String", 10};
 
     cout<<"First String :"<<sr.first<<endl;
     cout<<"Second Integer :"<<sr.second<<endl;
 
-    pair <int, pair <char, int>> p = {15, {'A', 25}};
+    pair <int, pair <char, int>> p = {15, {'A', 25}}; // here first is int & second is another pair
 
-    cout<<"First Integer :"<<p.first<<endl;
+    cout<<"First Integer :"<<p.first<<endl; //returns 15
 
-    cout<<"First part of pair :"<<p.second.first<<endl;
-    cout<<"Second Part of Pair :"<<p.second.second<<endl;
+    cout<<"First part of pair :"<<p.second.first<<endl; // returns 'A'
+    cout<<"Second Part of Pair :"<<p.second.second<<endl; // returns 25
 
-    vector <pair<int, int>> vec={{1,2}, {3,4}, {5,6}};
+    vector <pair<int, int>> vec={{1,2}, {3,4}, {5,6}}; // Array of pairs
 
     vec.push_back({7,8}); //here push_back function performs only insert operation and assumes that pair is already created and need to be inserted
     vec.emplace_back(9,10); //here emplace_back function will create in-place objects and make pair of the numbers and then insert
